91e3c2df7423666b0a5365d400e47826769041f2
Daniele-996 <danielesanza@gmail.com>
2025-07-04 12:03:11 +0200

    Initial commit

---
+++ src/test/java/danieleSanzari/ApplicationTest.java
@@ -0,0 +1,13 @@
+package danieleSanzari;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ApplicationTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
